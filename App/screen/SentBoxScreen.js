b9da5f472766882020c3061c10c25df9d9f4d39e
azharie_muhammad <you@example.com>
2019-08-05 15:27:34 +0700

    add facility name

--- App/screen/SentBoxScreen.js
+++ App/screen/SentBoxScreen.js
@@ -4,17 +4,26 @@ import {Container, Content, H3} from "native-base";
 import ListCase from "../component/ListCase";
 import {baseService} from "../services";
 import BoxHeader from "../component/BoxHeader";
+import {getUserDetail} from "../utils/storeUserDetail";
 
 const SentBoxScreen = () => {
 
     const [cases, setCases] = useState([]);
+    const [userDetail, setUserDetail] = useState({
+        address: "",
+        email: "",
+        first_name: "",
+        health_facility_name: "",
+        last_name: "",
+        phone_number: ""
+    })
 
     useEffect(() => {
         async function getCaseInfo() {
             await baseService().getAllCaseInformation().then(result => {
                 setCases(result.data)
             }).catch(e => {
-                setCases([])
+                setCases({})
             })
 
         }
@@ -22,9 +31,21 @@ const SentBoxScreen = () => {
         getCaseInfo();
     }, []);
 
+    useEffect(() => {
+        function userDetail() {
+            getUserDetail().then(detail => {
+                setUserDetail(detail)
+            }).catch(e => {
+                console.log('Something went wrong', e)
+            })
+        }
+
+        userDetail()
+    }, [])
+
     return (
         <Container>
-            <BoxHeader title={'Pustu Desa A'}/>
+            <BoxHeader title={userDetail.health_facility_name}/>
             <H3 style={{marginTop: 16, marginBottom: 16, padding:8}}>Laporan Terkirim</H3>
             <ListCase data={cases}/>
         </Container>
